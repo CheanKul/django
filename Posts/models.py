241a68ade31eaad119b4a15d5f64c7086e7af9e2
Chetan <son.chetanakulkarni@gmail.com>
2019-03-20 00:26:33 +0530

    Changes

--- Posts/models.py
+++ Posts/models.py
@@ -7,7 +7,7 @@ from djongo import models
 class Post(models.Model):
     Header = models.CharField(max_length=200)
     Content = models.TextField()
-    ImageURL = models.CharField(max_length=500)
+    ImageURL = models.TextField()
 
     def __str__(self):
         return self.Header
